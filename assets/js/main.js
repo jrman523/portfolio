a90406f86d210b8f5f44fb0c5b19c0daa624ea41
Jad El Ramadi <jrman523@users.noreply.github.com>
2019-03-20 04:35:08 -0400

    pushing the last function of the nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,18 @@ window.onload = function () {
 $(document).on('click', '.nav-item', function () {
     var active = $(this).attr('id');
     if (active === 'about') {
-        $('html, body').animate({
-            scrollTop: $("#about-section").offset().top
-        }, 1000);
+        $('html,body').animate({ scrollTop: $("#about-section").offset().top }, 1000);
         $('#portfolio').removeClass('active');
         $('#contact').removeClass('active');
         $('#about').addClass('active');
     } else if (active === 'portfolio') {
-        $('html, body').animate({
-            scrollTop: $("#portfolio-section").offset().top
-        }, 1000);
+        $('html,body').animate({ scrollTop: $("#portfolio-section").offset().top }, 1000);
         $('#about').removeClass('active');
         $('#contact').removeClass('active');
         $('#portfolio').addClass('active');
 
     } else if (active === 'contact') {
-        $('html, body').animate({
-            scrollTop: $("#contact-section").offset().top
-        }, 1000);
+        $('html,body').animate({ scrollTop: $("#contact-section").offset().top }, 1000);
         $('#about').removeClass('active');
         $('#portfolio').removeClass('active');
         $('#contact').addClass('active');
@@ -30,18 +24,21 @@ $(document).on('click', '.nav-item', function () {
     }
 });
 
-$(window).on('scroll', function () {
-    var set = $(this).attr('id');
+$(document).on('scroll', function () {
+    var aboutSec = $("#about-section").offset().top;
+    var portfolioSec = $("#portfolio-section").offset().top;
+    var contactSec = $("#contact-section").offset().top;
+    var set = $(document).scrollTop();
     console.log(set);
-    if (set === 'about-section') {
+    if (set > aboutSec && set < portfolioSec && set < contactSec) {
         $('#portfolio').removeClass('active');
         $('#contact').removeClass('active');
         $('#about').addClass('active');
-    } else if (set === 'portfolio-section') {
+    } else if (set > portfolioSec && set < contactSec) {
         $('#about').removeClass('active');
         $('#contact').removeClass('active');
         $('#portfolio').addClass('active');
-    } else if (set === 'contact-section') {
+    } else if (set > contactSec) {
         $('#about').removeClass('active');
         $('#portfolio').removeClass('active');
         $('#contact').addClass('active');
