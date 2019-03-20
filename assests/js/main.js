04bbc1c2525b50da236e74026a5b15c5b44c88d3
jad <jadelramadi@gmail.com>
2019-03-19 22:48:07 -0400

    fixed css bug

--- assests/js/main.js
+++ assests/js/main.js
@@ -5,30 +5,32 @@ window.onload = function () {
 $(document).on('click', '.nav-item', function () {
     var active = $(this).attr('id');
     if (active === 'about') {
+        $('html, body').animate({
+            scrollTop: $("#about-section").offset().top
+        }, 1000);
         $('#portfolio').removeClass('active');
         $('#contact').removeClass('active');
         $('#about').addClass('active');
-        $('html, body').animate({
-            scrollTop: $("#about-section").offset().top
-            }, 1000);
     } else if (active === 'portfolio') {
+        $('html, body').animate({
+            scrollTop: $("#portfolio-section").offset().top
+        }, 1000);
         $('#about').removeClass('active');
         $('#contact').removeClass('active');
         $('#portfolio').addClass('active');
-        $('html, body').animate({
-            scrollTop: $("#portfolio-section").offset().top
-            }, 1000);
+
     } else if (active === 'contact') {
+        $('html, body').animate({
+            scrollTop: $("#contact-section").offset().top
+        }, 1000);
         $('#about').removeClass('active');
         $('#portfolio').removeClass('active');
         $('#contact').addClass('active');
-        $('html, body').animate({
-            scrollTop: $("#contact-section").offset().top
-            }, 1000);
+
     }
 });
 
-$(window).on('scroll', '.container', function () {
+$(window).on('scroll', function () {
     var set = $(this).attr('id');
     console.log(set);
     if (set === 'about-section') {
